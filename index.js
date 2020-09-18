051c2e3fc92b4eb4cb04cab0f25af321abf468da
krukd <krukdmitriyv@gmail.com>
2020-09-18 15:47:10 +0300

    add new button deposit and url

--- index.js
+++ index.js
@@ -62,7 +62,9 @@ bot.onText(/\/apikey (.+)/ ,(msg,[source, match])=>{
 bot.onText(/\/myapikey/, function onEchoText(msg) {
     const {chat:{id,username}} = msg;
 
-    bot.sendMessage(id, myCache.get(id).apikey,inline_button());
+    $paymentUrl = 'https://qiwi.com/payment/form/99?blocked%5B0%5D=account&amountFraction=75&extra%5B%27account%27%5D=79776962780&extra%5B%27comment%27%5D='+ 'hub_'+myCache.get(id).apikey +'&amountInteger=25.75&blocked%5B1%5D=sum&currency=RUB'
+
+    bot.sendMessage(id, $paymentUrl,inline_button());
 });
 //-------------------------------------------------------//
 
@@ -256,9 +258,9 @@ function inline_button(type) {
             opts = {
                 reply_markup: JSON.stringify({
                     keyboard: [
+                        ['Пополнить счет'],
                         ['Цена','Баланс'],
-                        ['Заказать номер'],
-                        ['Пополнить счет']
+                        ['Заказать номер']
                     ],
                     'resize_keyboard': true
                 })
